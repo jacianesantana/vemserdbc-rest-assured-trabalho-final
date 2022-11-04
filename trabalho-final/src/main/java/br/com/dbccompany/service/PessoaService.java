32af7cd824348d4b562a0290bda1b1bb5774cb46
Eduardo <eduhmq@gmail.com>
2022-11-04 01:40:21 -0300

    testes de pessoas

--- trabalho-final/src/main/java/br/com/dbccompany/service/PessoaService.java
+++ trabalho-final/src/main/java/br/com/dbccompany/service/PessoaService.java
@@ -2,10 +2,14 @@ package br.com.dbccompany.service;
 
 import br.com.dbccompany.dto.PessoaDTO;
 import br.com.dbccompany.dto.RelatorioDTO;
+import br.com.dbccompany.dto.ResponseDTO;
 import br.com.dbccompany.utils.Login;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.parsing.Parser;
+import io.restassured.response.Response;
+
+import java.io.IOException;
 
 import static io.restassured.RestAssured.*;
 
@@ -51,12 +55,44 @@ public class PessoaService {
                 ;
         return result;
     }
+    public Response naoCriarPessoaVazio(String jsonBody) {
+        Response result =
+                given()
+                        .log().all()
+                        .header("Authorization", tokenAdmin)
+                        .contentType(ContentType.JSON)
+                        .body(jsonBody)
+
+                        .when()
+                        .post(baseUri + "/pessoa")
+
+                        .then()
+                        .extract().response()
+                ;
+        return result;
+    }
+    public Response deletarPorId(String id) {
+        Response result =
+                given()
+                        .log().all()
+                        .header("Authorization", tokenAdmin)
+
+                        .when()
+                        .delete(baseUri + "/pessoa/" + id)
+
+                        .then()
+
+                        .extract().response()
+                ;
+        return result;
+    }
 
     public PessoaDTO listarPorCpf(String cpf) {
 
         PessoaDTO result =
                 given()
                         .header("Authorization", tokenAdmin)
+                        .pathParam("cpf", cpf)
 
                         .when()
                         .get(baseUri + "/pessoa/{cpf}/cpf")
@@ -65,6 +101,80 @@ public class PessoaService {
                         .log().all()
                         .statusCode(200)
                         .extract().as(PessoaDTO.class)
+                ;
+        return result;
+    }
+
+    public Response listarCpfInexistente(String cpf) {
+        Response result =
+                given()
+                        .header("Authorization", tokenAdmin)
+                        .pathParam("cpf", cpf)
+
+                        .when()
+                        .get(baseUri + "/pessoa/{cpf}/cpf")
+
+                        .then()
+                        .log().all()
+                        .statusCode(403)
+                        .extract().response()
+                ;
+        return result;
+    }
+
+    public PessoaDTO atualizarPessoa(String jsonBodyAtualizado, String id) {
+        PessoaDTO result =
+                given()
+                        .log().all()
+                        .header("Authorization", tokenAdmin)
+                        .pathParam("id", id)
+                        .contentType(ContentType.JSON)
+                        .body(jsonBodyAtualizado)
+
+                        .when()
+                        .put(baseUri + "/pessoa/{id}")
+
+                        .then()
+                        .log().all()
+                        .extract().as(PessoaDTO.class)
+                ;
+        return result;
+    }
+
+    public Response atualizarPessoaIvalida(String jsonBodyInvalido, String id) {
+        Response result =
+                given()
+                        .log().all()
+                        .header("Authorization", tokenAdmin)
+                        .pathParam("id", id)
+                        .contentType(ContentType.JSON)
+                        .body(jsonBodyInvalido)
+
+                        .when()
+                        .put(baseUri + "/pessoa/{id}")
+
+                        .then()
+                        .log().all()
+                        .extract().response()
+                ;
+        return result;
+    }
+
+    public Response atualizarPessoaIdInvalido(String idInvalido, String jsonBodyAtualizado) {
+        Response result =
+                given()
+                        .log().all()
+                        .header("Authorization", tokenAdmin)
+                        .pathParam("idInvalido", idInvalido)
+                        .contentType(ContentType.JSON)
+                        .body(jsonBodyAtualizado)
+
+                        .when()
+                        .put(baseUri + "/pessoa/{idInvalido}")
+
+                        .then()
+                        .log().all()
+                        .extract().response()
                 ;
         return result;
     }
--- trabalho-final/src/test/java/br/com/dbccompany/aceitacao/PessoaAceitacaoTest.java
+++ trabalho-final/src/test/java/br/com/dbccompany/aceitacao/PessoaAceitacaoTest.java
@@ -2,7 +2,10 @@ package br.com.dbccompany.aceitacao;
 
 import br.com.dbccompany.dto.PessoaDTO;
 import br.com.dbccompany.dto.RelatorioDTO;
+import br.com.dbccompany.dto.ResponseDTO;
 import br.com.dbccompany.service.PessoaService;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -26,24 +29,136 @@ public class PessoaAceitacaoTest {
     @Test
     public void testeDeveAdicionarPessoa() throws IOException{
 
-        String jsonBody = lerJson("src/test/resources/data/pessoa1.json");
+        String jsonBody = lerJson("src/main/resources/data/pessoa1.json");
 
 
         PessoaDTO resultService = service.criarPessoa(jsonBody);
 
-        Assert.assertEquals(resultService.getNome(), "Marta");
-        Assert.assertEquals(resultService.getEmail(), "martagolpe@gmail.com");
-        Assert.assertEquals(resultService.getDataNascimento(), "1975-10-10");
-        Assert.assertEquals(resultService.getIdPessoa(), 10);
+        Assert.assertEquals(resultService.getNome(), "Marta Golpista");
+        Assert.assertEquals(resultService.getEmail(), "marta@golpista.com");
+        Assert.assertEquals(resultService.getDataNascimento(), "2000-05-09");
 
+
+        Response resultService1 = service.deletarPorId(resultService.getIdPessoa());
+        Assert.assertEquals(resultService1.getStatusCode(), 200);
+    }
+
+    @Test
+    public void testeDeveTentarCriarPessoaSemDados() throws IOException {
+        String jsonBody = "";
+
+        Response resultService = service.naoCriarPessoaVazio(jsonBody);
+
+        Assert.assertEquals(resultService.getStatusCode(), 400);
 
     }
 
     @Test
-    public void testeDeveConsultarPessoaPorCpf() {
-        String cpf = "555";
-        PessoaDTO resultService = service.listarPorCpf(cpf);
+    public void testeDeveDeletarPessoa() throws IOException {
+        //Criando nova pessoa
+        String jsonBody = lerJson("src/main/resources/data/pessoa1.json");
+        PessoaDTO resultService = service.criarPessoa(jsonBody);
 
-        Assert.assertEquals(resultService.getCpf(), "");
+        Assert.assertEquals(resultService.getNome(), "Marta Golpista");
+        Assert.assertEquals(resultService.getEmail(), "marta@golpista.com");
+        Assert.assertEquals(resultService.getDataNascimento(), "2000-05-09");
+
+        //Delete
+        Response resultService1 = service.deletarPorId(resultService.getIdPessoa());
+        Assert.assertEquals(resultService1.getStatusCode(), 200);
     }
+    @Test
+    public void testeTentarDeletarPessoaInexistente() throws IOException {
+        String idInexistente = "545454";
+
+        Response resultService = service.deletarPorId(idInexistente);
+
+        Assert.assertEquals(resultService.getStatusCode(), 404);
+    }
+    @Test
+    public void testeDeveConsultarPessoaPorCpf() throws IOException{
+        String jsonBody = lerJson("src/main/resources/data/pessoa2.json");
+        PessoaDTO responseService = service.criarPessoa(jsonBody);
+
+        String cpf = "741258963";
+        PessoaDTO responseService1 = service.listarPorCpf(cpf);
+        Assert.assertEquals(responseService1.getNome(), "Igor Dinho");
+        Assert.assertEquals(responseService1.getCpf(), "741258963");
+
+        Response resultService = service.deletarPorId(responseService.getIdPessoa());
+        Assert.assertEquals(resultService.getStatusCode(), 200);
+    }
+    @Test
+    public void testeDeveConsultarCpfInexistente() throws IOException {
+
+        String cpf = "414475847";
+        Response resultService = service.listarCpfInexistente(cpf);
+        Assert.assertEquals(resultService.getStatusCode(), 403);
+    }
+
+    @Test
+    public void testeDeveAtualizarPessoa() throws IOException {
+        //Criando a pessoa
+        String jsonBody = lerJson("src/main/resources/data/pessoa1.json");
+        PessoaDTO resultService = service.criarPessoa(jsonBody);
+        Assert.assertEquals(resultService.getNome(), "Marta Golpista");
+        String id = resultService.getIdPessoa();
+
+        //Atualizando pessoa
+        String jsonBodyAtualizado = lerJson("src/main/resources/data/pessoa3.json");
+
+        PessoaDTO resultService1 = service.atualizarPessoa(jsonBodyAtualizado, id);
+        Assert.assertEquals(resultService1.getNome(), "Tais Condida");
+        Assert.assertEquals(resultService1.getDataNascimento(), "1995-07-05");
+        Assert.assertEquals(resultService1.getCpf(), "855462");
+        Assert.assertEquals(resultService1.getEmail(), "tais@condida.com");
+
+
+        //Deletando pessoa
+        Response deleteResult = service.deletarPorId(resultService1.getIdPessoa());
+        Assert.assertEquals(deleteResult.getStatusCode(), 200);
+    }
+
+    @Test
+    public void testeDeveTentarAtualizarPessoaComParametrosVazios() throws IOException {
+
+        //Criando a pessoa
+        String jsonBody = lerJson("src/main/resources/data/pessoa1.json");
+        PessoaDTO resultService = service.criarPessoa(jsonBody);
+        Assert.assertEquals(resultService.getNome(), "Marta Golpista");
+        String id = resultService.getIdPessoa();
+
+        //Atualizando pessoa
+        String jsonBodyInvalido = lerJson("src/main/resources/data/pessoaInvalida.json");
+
+        Response resultService1 = service.atualizarPessoaIvalida(jsonBodyInvalido, id);
+        Assert.assertEquals(resultService1.getStatusCode(), 400);
+
+
+        //Deletando pessoa
+        Response deleteResult = service.deletarPorId(resultService.getIdPessoa());
+        Assert.assertEquals(deleteResult.getStatusCode(), 200);
+    }
+
+    @Test
+    public void testeDeveTentarAtualizarPessoaPassandoIdInvalido() throws IOException {
+
+        //Criando a pessoa
+        String jsonBody = lerJson("src/main/resources/data/pessoa1.json");
+        PessoaDTO resultService = service.criarPessoa(jsonBody);
+        Assert.assertEquals(resultService.getNome(), "Marta Golpista");
+        String id = resultService.getIdPessoa();
+
+        //Atualizando pessoa
+        String idInvalido = "1231223";
+        String jsonBodyAtualizado = lerJson("src/main/resources/data/pessoa3.json");
+        Response responseService = service.atualizarPessoaIdInvalido(idInvalido, jsonBodyAtualizado);
+        Assert.assertEquals(responseService.getStatusCode(), 404);
+
+        //Deletando pessoa
+        Response deleteResult = service.deletarPorId(resultService.getIdPessoa());
+        Assert.assertEquals(deleteResult.getStatusCode(), 200);
+    }
+
+
 }
