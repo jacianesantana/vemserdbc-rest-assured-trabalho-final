55613ed6b50953d80b9f8a563af6a1bf6f323407
Eduardo <eduhmq@gmail.com>
2022-11-04 23:37:14 -0300

    testes finais

---
+++ trabalho-final/src/main/java/br/com/dbccompany/dto/ContentDTO.java
@@ -0,0 +1,14 @@
+package br.com.dbccompany.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties
+public class ContentDTO {
+    private String nome;
+    private String dataNascimento;
+    private String cpf;
+    private String email;
+    private String idPessoa;
+}
---
+++ trabalho-final/src/main/java/br/com/dbccompany/dto/PessoaListaCompletaDTO.java
@@ -0,0 +1,17 @@
+package br.com.dbccompany.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties
+public class PessoaListaCompletaDTO {
+
+    private String totalElements;
+    private String totalPages;
+    private String page;
+    private String size;
+    private ContentDTO[] content;
+
+}
